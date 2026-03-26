e53c60c0becb848570a782b021316d37a6a8f349
Tanjya Akther <tanjya26@gmail.com>
2026-03-26 12:19:13 +0000

    style homepage, globe & achievements

--- public/script/globe.js
+++ public/script/globe.js
@@ -4,8 +4,8 @@ const globe = Globe()(globeContainer)
   .globeImageUrl("//unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
   .backgroundColor("rgba(0,0,0,0)");
 
-globe.width(550);
-globe.height(550);
+globe.width(615);
+globe.height(615);
 globe.pointOfView({ altitude: 1.6 });
 
 globe.controls().autoRotate = true;
